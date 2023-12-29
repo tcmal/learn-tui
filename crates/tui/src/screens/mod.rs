2f389dd86197e58f2263a2cfa15426fef483de31
Aria <me@aria.rip>
2023-12-29 00:20:58 +0000

    add border around currently focused screen

--- crates/tui/src/screens/mod.rs
+++ crates/tui/src/screens/mod.rs
@@ -1,6 +1,11 @@
 use anyhow::Result;
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
-use ratatui::{prelude::*, Frame};
+use log::debug;
+use ratatui::{
+    prelude::*,
+    widgets::{Block, Borders},
+    Frame,
+};
 
 use crate::{app::App, store::Store};
 
@@ -24,14 +29,48 @@ trait Page {
 
 impl App {
     pub fn draw(&mut self, frame: &mut Frame) {
+        let size = frame.size();
+
+        // area for content
+        let content_rect = Rect {
+            x: size.x + 1,
+            y: size.y + 1,
+            width: size.width - 2,
+            height: size.height - 2,
+        };
+
         let layout = Layout::new(
             Direction::Horizontal,
-            [Constraint::Percentage(50), Constraint::Percentage(50)],
+            [
+                Constraint::Percentage(30),
+                Constraint::Length(1),
+                Constraint::Percentage(70),
+            ],
         )
-        .split(frame.size());
+        .split(content_rect);
+
+        // area for focus rectangle to be drawn around
+        let focus_rect = if !self.viewer_focused {
+            Rect {
+                x: size.x,
+                y: size.y,
+                width: layout[2].x - size.x,
+                height: size.height,
+            }
+        } else {
+            Rect {
+                x: layout[1].x,
+                y: size.y,
+                width: size.width - layout[1].x,
+                height: size.height,
+            }
+        };
+
+        debug!("content: {:?}, focus: {:?}", content_rect, focus_rect);
 
         self.navigation.draw(&self.store, frame, layout[0]);
-        self.viewer.draw(&self.store, frame, layout[1]);
+        self.viewer.draw(&self.store, frame, layout[2]);
+        frame.render_widget(Block::default().borders(Borders::ALL), focus_rect);
     }
 
     pub fn handle_key(&mut self, key: KeyEvent) -> Result<()> {
