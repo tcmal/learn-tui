ce9a114df4b0d9c6a15a8c02ac63d8e273d13bd7
Aria Shrimpton <me@aria.rip>
2024-01-23 10:51:26 +0000

    fix invalidation bug in viewer

--- crates/tui/src/panes/viewer.rs
+++ crates/tui/src/panes/viewer.rs
@@ -40,40 +40,52 @@ impl Viewer {
 
 impl Pane for Viewer {
     fn draw(&mut self, store: &Store, frame: &mut Frame, area: Rect) {
-        let rendered = self.cached_render.clone().unwrap_or_else(|| {
-            self.cached_render = Some(match self.show {
-                Document::Content(idx) => self.render_content(store, idx),
-                Document::Welcome => Paragraph::new(Into::<Text>::into(vec![
-                    vec!["Welcome to learn-tui!\n".blue().bold()].into(),
-                    vec![
-                        "Use ".into(),
-                        "j/k or ↓/↑".blue(),
-                        " to navigate up and down, then ".into(),
-                        "Enter".blue(),
-                        " to select an item.\n".into(),
-                    ]
-                    .into(),
-                    vec![
-                        "When an item is selected, you can scroll the viewer pane using ".into(),
-                        "j/k ↓/↑ g/G PgUp/PgDn".blue(),
-                        " and go back to the navigation pane with ".into(),
-                        "q".blue(),
-                        ".".into(),
-                    ]
-                    .into(),
-                    vec![
-                        "At any point, use ".into(),
-                        "b".blue(),
-                        " to try to open the selected item in your browser.\n".into(),
-                    ]
-                    .into(),
-                    vec!["Use ".into(), "Ctrl-C".blue(), " to quit.".into()].into(),
-                ]))
-                .wrap(Wrap { trim: false }),
+        let rendered = self
+            .cached_render
+            .clone()
+            .unwrap_or_else(|| match self.show {
+                Document::Content(idx) => {
+                    if let Some(p) = self.render_content(store, idx) {
+                        self.cached_render = Some(p.clone());
+                        p
+                    } else {
+                        // dont cache
+                        Paragraph::new("Loading...")
+                    }
+                }
+                Document::Welcome => {
+                    let p = Paragraph::new(Into::<Text>::into(vec![
+                        vec!["Welcome to learn-tui!\n".blue().bold()].into(),
+                        vec![
+                            "Use ".into(),
+                            "j/k or ↓/↑".blue(),
+                            " to navigate up and down, then ".into(),
+                            "Enter".blue(),
+                            " to select an item.\n".into(),
+                        ]
+                        .into(),
+                        vec![
+                            "When an item is selected, you can scroll the viewer pane using "
+                                .into(),
+                            "j/k ↓/↑ g/G PgUp/PgDn".blue(),
+                            " and go back to the navigation pane with ".into(),
+                            "q".blue(),
+                            ".".into(),
+                        ]
+                        .into(),
+                        vec![
+                            "At any point, use ".into(),
+                            "b".blue(),
+                            " to try to open the selected item in your browser.\n".into(),
+                        ]
+                        .into(),
+                        vec!["Use ".into(), "Ctrl-C".blue(), " to quit.".into()].into(),
+                    ]))
+                    .wrap(Wrap { trim: false });
+                    self.cached_render = Some(p.clone());
+                    p
+                }
             });
-
-            self.cached_render.clone().unwrap()
-        });
 
         let line_count = rendered.line_count(area.width);
         self.jump_y_offset = area.height / 2;
@@ -140,28 +152,32 @@ impl Pane for Viewer {
 }
 
 impl Viewer {
-    fn render_content(&mut self, store: &Store, content_idx: ContentIdx) -> Paragraph<'static> {
+    fn render_content(
+        &mut self,
+        store: &Store,
+        content_idx: ContentIdx,
+    ) -> Option<Paragraph<'static>> {
         let content = store.content(content_idx);
         match &content.payload {
             ContentPayload::Page => match store.page_text(content_idx) {
                 Some(text) => {
                     self.cached_render = Some(bbml::render(text));
-                    self.cached_render.clone().unwrap()
+                    Some(self.cached_render.clone().unwrap())
                 }
                 None => {
                     store.request_page_text(content_idx);
-                    Paragraph::new("Loading...")
+                    None
                 }
             },
-            ContentPayload::Link(l) => Paragraph::new(format!("Link to {}. Open with b", l)),
-            ContentPayload::Folder => Paragraph::new("Folder"),
-            ContentPayload::Other(o) => Paragraph::new(vec![
+            ContentPayload::Link(l) => Some(Paragraph::new(format!("Link to {}. Open with b", l))),
+            ContentPayload::Folder => Some(Paragraph::new("Folder")),
+            ContentPayload::Other(o) => Some(Paragraph::new(vec![
                 Line::styled(
                     format!("Unknown content type: '{}'", o),
                     Style::new().fg(Color::Red).add_modifier(Modifier::BOLD),
                 ),
-                Line::raw("Complain to Aria, and in the meantime open in your browser with b."),
-            ]),
+                Line::raw("File an issue, and in the meantime open in your browser with b."),
+            ])),
         }
     }
 }
