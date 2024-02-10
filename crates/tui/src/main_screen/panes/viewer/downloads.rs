429576a69713f1badc055a4b37851c395743e330
Aria Shrimpton <me@aria.rip>
2024-02-10 15:03:32 +0000

    show empty downloads screen when none started

--- crates/tui/src/main_screen/panes/viewer/downloads.rs
+++ crates/tui/src/main_screen/panes/viewer/downloads.rs
@@ -11,28 +11,30 @@ pub struct DownloadsViewer {}
 
 impl Pane for DownloadsViewer {
     fn draw(&mut self, store: &Store, frame: &mut Frame, area: Rect) {
-        let p = Paragraph::new(
-            store
-                .download_queue()
-                .flat_map(|(req, state)| {
+        let lines = store
+            .download_queue()
+            .flat_map(|(req, state)| {
+                vec![
                     vec![
-                        vec![
-                            req.orig_filename.to_string().blue(),
-                            match &state {
-                                DownloadState::Queued => " - Queued".gray(),
-                                DownloadState::InProgress(p) => {
-                                    format!(" - {:.2}%", p * 100.0).blue()
-                                }
-                                DownloadState::Completed => " - Completed".green(),
-                                DownloadState::Errored(e) => format!(" - {e}").red(),
-                            },
-                        ]
-                        .into(),
-                        vec![req.dest.to_string().gray()].into(),
+                        req.orig_filename.to_string().blue(),
+                        match &state {
+                            DownloadState::Queued => " - Queued".gray(),
+                            DownloadState::InProgress(p) => format!(" - {:.2}%", p * 100.0).blue(),
+                            DownloadState::Completed => " - Completed".green(),
+                            DownloadState::Errored(e) => format!(" - {e}").red(),
+                        },
                     ]
-                })
-                .collect::<Vec<Line>>(),
-        );
+                    .into(),
+                    vec![req.dest.to_string().gray()].into(),
+                ]
+            })
+            .collect::<Vec<Line>>();
+
+        let p = if lines.is_empty() {
+            Paragraph::new("No downloads started.")
+        } else {
+            Paragraph::new(lines)
+        };
 
         frame.render_widget(p, area);
     }
